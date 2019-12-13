420b50c8bb095ff20239658bb59204bebfdd0775
Connor Flanigan <connorflanigan@gmail.com>
2019-12-12 19:22:28 -0500

    day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 def createimage(inputString, width, height):
     rawData = np.array(list(inputString)).astype(np.uint8)
     image = rawData.reshape((25, 6, len(rawData)//(width*height)), order='F')
@@ -13,6 +15,15 @@ if __name__ == '__main__':
     minZeros = np.argmin(nZeros)
     minZeroLayer = image[:, :, minZeros]
     print(np.sum(minZeroLayer == 1, (0, 1))*np.sum(minZeroLayer == 2, (0, 1)))
+
+    newImage = np.ones((25, 6))*2
+    for i in range(image.shape[2]):
+        for j in range(25):
+            for k in range(6):
+                if newImage[j, k] == 2:
+                    newImage[j, k] = image[j, k, i]
+    plt.pcolormesh(newImage)
+    plt.show()
 
 
 
